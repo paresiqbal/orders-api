66e75cf59a33b13daeb0ff2f3f270e300e71d818
paresiqbal <thecrowdel@gmail.com>
2024-01-07 00:40:19 +0700

    Add redis Add connection to redis Add timeout to shutdown

--- application/app.go
+++ application/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -34,12 +35,31 @@ func (a *App) Start(ctx context.Context) error {
 		return fmt.Errorf("failed to ping redis: %w", err)
 	}
 
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis connection", err)
+		}
+	}()
+
 	fmt.Println("Server is running on port 3000")
 
-	err = server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
-	}
+	ch := make(chan error, 1)
 
-	return nil
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err := <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
+	}
 }
